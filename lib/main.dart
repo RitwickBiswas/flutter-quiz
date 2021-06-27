908c1457e7333fce5826d4994ffb4a297c5f7067
Ritwick Biswas <ritwick.biswas95@gmail.com>
2021-06-27 12:21:58 +0530

    buttons and functions

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,43 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
+  void answerQuestion(){
+    print('Answer chosen !');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color ?',
+      'What\'s your favorite animal ?',
+    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('My First App'),),
-        body: Text('This is my default text'),
-      ),);
+        appBar: AppBar(
+          title: Text('Personality Quiz'),
+        ),
+        body: Column(
+          children: [
+            Text('The questions !'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print("Answer 2 chosen"),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => {
+                print('Answer 3 long chosen'),
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
 }
